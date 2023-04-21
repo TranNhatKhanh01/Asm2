4cb7f101d97053d2a290c2f0ab58370aa839a392
Khanhtngcc19208 <91667470+TranNhatKhanh01@users.noreply.github.com>
2023-04-21 12:44:29 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$server = "localhost";
-$username ="root";
-$password = "";
-$db = "asm";
+$server = "us-cdbr-east-06.cleardb.net";
+$username ="b5f754e0ff5e7f";
+$password = "a5bb5d50";
+$db = "heroku_9a6ee2442b34b52";
 
 $conn = mysqli_connect($server,$username,$password,$db);
 if($conn->connect_error){
